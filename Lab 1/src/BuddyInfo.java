7ce02162a41a047b73ce02e6c8ba58f24e1eaaaa
JamesBian1984 <168999335+JamesBian1984@users.noreply.github.com>
2025-09-13 16:30:18 -0400

    Introduced Address Book.

---
+++ Lab 1/src/BuddyInfo.java
@@ -0,0 +1,29 @@
+public class BuddyInfo {
+
+    private String name;
+    private int phone_number;
+    private String address;
+
+
+    public BuddyInfo(String name, int phone_number, String address) {
+        this.name = name;
+        this.phone_number = phone_number;
+        this.address = address;
+    }
+
+    public BuddyInfo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+    public static void main(String[] args) {
+        BuddyInfo buddy1 = new BuddyInfo("Homer", 1800, "addr");
+        System.out.println("Hello, " + buddy1.name);
+
+
+    }
+}
+
+
+
