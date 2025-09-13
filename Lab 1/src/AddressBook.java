cf6a5b6a78022b0d19f92c38dafa6caec43fe35f
JamesBian1984 <168999335+JamesBian1984@users.noreply.github.com>
2025-09-13 16:37:31 -0400

    Implemented addBuddy and removeBuddy.

--- Lab 1/src/AddressBook.java
+++ Lab 1/src/AddressBook.java
@@ -1,5 +1,16 @@
 public class AddressBook {
+
+    private void addBuddy(BuddyInfo bud) {
+        return;
+    }
+    private void removeBuddy(BuddyInfo bud) {
+        return;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("John", 123, "Canada");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
